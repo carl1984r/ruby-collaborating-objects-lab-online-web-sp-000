5b4c3db1fc596c6a0b38fce68dbef2b8b717bfda
Bry Cox <carl.cc@gmail.com>
2019-03-15 18:29:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
 
   def artist_name=(name)
        @artist = Artist.find_or_create_by_name(name)
-       @artis.add_song(self)
+       @artist.add_song(self)
      end
 
   def self.new_by_filename(x)
